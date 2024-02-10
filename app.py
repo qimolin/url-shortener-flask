c5c881de507d8225a5199f23d8796f75cc248580
Qi-Mo Lin <qimolin9@gmail.com>
2024-02-10 16:11:49 +0100

    Add missing endpoints

--- app.py
+++ app.py
@@ -1,25 +1,58 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from url import URL
 
 app = Flask(__name__)
 url_processor = URL()
 
-@app.route('/')
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/shorten', methods=['POST'])
+@app.post('/')
 def shorten_url():
 
     data = request.get_json()
-    original_url = data.get('url')
+    original_url = data.get('value')
     shortened_url = url_processor.generate_shortened_url(original_url)
+    
+    status_code = 201 if shortened_url != "Invalid URL" else 400
 
-    return(shortened_url)
+    return {"id": shortened_url}, status_code
 
-@app.route('/original', methods=['GET'])
-def get_original_url():
-    return "TODO"
+@app.get('/')
+def get_all_urls():
+    all_urls = url_processor.get_all_urls()
+    return all_urls, 200
+
+@app.delete('/')
+def delete_all_urls():
+    deleted = url_processor.delete_all_urls()
+    return deleted, 404
+
+@app.get('/<id>')
+def get_original_url(id):
+    original_url = url_processor.get_original_url(id)
+    
+    if original_url:
+        # return redirect(original_url, code=301)
+        return {"value": original_url}, 301
+    return "URL not found", 404
+
+@app.put('/<id>')
+def update_original_url(id):
+    new_url = request.get_json().get('url')
+    updated_url = url_processor.update_original_url(id, new_url)
+    
+    if updated_url == "Invalid URL":
+        status_code = 400
+    elif updated_url == "URL not found":
+        status_code = 404
+    else:
+        status_code = 200
+        
+    return {"value":updated_url}, status_code
+
+@app.delete('/<id>')
+def delete_original_url(id):
+    deleted = url_processor.delete_original_url(id)
+    status_code = 204 if deleted == "URL deleted" else 404
+    return deleted, status_code
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- url.py
+++ url.py
@@ -4,21 +4,58 @@ import redis
 class URL:
     
     BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-    def __init__(self):
-        self.redis = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+    redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
     def generate_shortened_url(self, url):
         if self.__is_valid(url):
 
             # create key to hold ID incrementer
-            url_id = self.redis.incr('url_counter')
+            url_id = self.redis_client.incr('url_counter')
 
             # save mapping
             shortened_url = self.__encode_base62(url_id)
-            self.redis.set(shortened_url, url)
+            self.redis_client.set(shortened_url, url)
 
             return shortened_url
+        else:
+            return "Invalid URL"
+        
+    def get_original_url(self, shortened_url):
+        return self.redis_client.get(shortened_url)
+
+    def get_all_urls(self):
+        keys = self.redis_client.keys('*')
+        
+        key_value_pairs = dict()
+        
+        for key in keys:
+            if key != 'url_counter':
+                key_value_pairs[f'http://localhost:5000/{key}'] = self.redis_client.get(key)
+        
+        return key_value_pairs
+    
+    def delete_all_urls(self):
+        keys = self.redis_client.keys('*')
+        for key in keys:
+            if key != 'url_counter':
+                self.redis_client.delete(key)
+        self.redis_client.set('url_counter', 0)
+        
+        return "All URLs deleted"
+    
+    def delete_original_url(self, shortened_url):
+        url = self.redis_client.get(shortened_url)
+        if url == None:
+            return "URL not found"
+        self.redis_client.delete(shortened_url)
+        return "URL deleted"
+    
+    def update_original_url(self, shortened_url, new_url):
+        if (self.redis_client.get(shortened_url) == None):
+            return "URL not found"
+        if self.__is_valid(new_url):
+            self.redis_client.set(shortened_url, new_url)
+            return "URL updated"
         else:
             return "Invalid URL"
 
